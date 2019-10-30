d4db9ca97e6543a3bc173964628a799c3a1d961c
lcastelan <ldcv@localhost.localdomain>
2019-10-29 18:34:00 -0600

    adding pip information and py files

---
+++ youtubeTutorial/app_modules.py
@@ -0,0 +1,17 @@
+
+
+#Import modules.py code
+import modules
+
+#Reference modules file and use the beatles object from there
+print( modules.beatles )
+
+print( modules.roll_dice(9) )
+
+print( modules.feet_in_mile )
+
+
+#Check the following url for important modules
+#Current modules that are available in Python
+#http://docs.python.org/3/py-modindex.html
+
